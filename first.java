02f339cbf7a1be7323c4889ffb5119393b2fddd3
prateekkumaryadav <prateek.yadav2006@gmail.com>
2026-01-17 12:02:03 +0530

    updated first.java

--- first.java
+++ first.java
@@ -1,5 +1,5 @@
 public class first{
   public static void main(String[] args) {
-      System.out.println("hello world i am trying out jenkins");
+      System.out.println("hello world i am trying jenkins");
   }
 }
